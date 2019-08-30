94824e237201e6c159b271aea83e10de65e4f5fc
Ariel Bandeira <arieltwain@gmail.com>
2019-08-30 16:10:41 -0300

    Add files via upload

--- QuartaQuestão/d/script.js
+++ QuartaQuestão/d/script.js
@@ -1,18 +1,47 @@
 var stream = document.querySelector('.gallery__stream');
 var items = document.querySelectorAll('.gallery__item');
-var checkbox = document.querySelector("#hover");
+var checkbox = document.getElementById("myCheckbox")
 
 var prev = document.querySelector('.gallery__prev');
 var next = document.querySelector('.gallery__next');
 
-checkbox.addEventListener("change", function myFunction() {
-  prev.addEventListener("click", function() {
-      stream.insertBefore(items[items.length - 1], items[0]);
-      items = document.querySelectorAll('.gallery__item');
-    });
 
-  next.addEventListener("click", function() {
-      stream.appendChild(items[0]);
-      items = document.querySelectorAll('.gallery__item');
-  });
+checkbox.addEventListener("change", function() {
+  if(checkbox.checked != true){
+    prev.addEventListener("click", function() {
+         stream.insertBefore(items[items.length - 1], items[0]);
+         items = document.querySelectorAll('.gallery__item');
+       });
+
+     next.addEventListener("click", function() {
+         stream.appendChild(items[0]);
+         items = document.querySelectorAll('.gallery__item');
+     });
+  }else if(checkbox.checked === true){
+    prev.addEventListener("mouseover", function() {
+         stream.insertBefore(items[items.length - 1], items[0]);
+         items = document.querySelectorAll('.gallery__item');
+       });
+
+     next.addEventListener("mouseover", function() {
+         stream.appendChild(items[0]);
+         items = document.querySelectorAll('.gallery__item');
+     });
+  }
+
 });
+
+
+
+      //
+      //
+      //
+      // prev.addEventListener("mouseover", function() {
+      //     stream.insertBefore(items[items.length - 1], items[0]);
+      //     items = document.querySelectorAll('.gallery__item');
+      //   });
+      //
+      // next.addEventListener("mouseover", function() {
+      //     stream.appendChild(items[0]);
+      //     items = document.querySelectorAll('.gallery__item');
+      // });
