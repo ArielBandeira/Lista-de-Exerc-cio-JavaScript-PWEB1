2656d02634e1534710e697ddd177f84e85b95813
Ariel Bandeira <arieltwain@gmail.com>
2019-09-02 14:05:09 -0300

    Add files via upload

--- QuartaQuestão/a/script.js
+++ QuartaQuestão/a/script.js
@@ -1,15 +1,26 @@
-var stream = document.querySelector('.gallery__stream');
-var items = document.querySelectorAll('.gallery__item');
 
-var prev = document.querySelector('.gallery__prev');
-var next = document.querySelector('.gallery__next');
+const galeria = function () {
+    let that = this;
+   this.stream = document.querySelector('.gallery__stream');
+   this.items = document.querySelectorAll('.gallery__item');
 
-prev.addEventListener('click', function() {
-    stream.insertBefore(items[items.length - 1], items[0]);
-    items = document.querySelectorAll('.gallery__item');
-  });
+   this.btnPrev = document.querySelector('.gallery__prev');
+   this.btnNext = document.querySelector('.gallery__next');
 
-next.addEventListener('click', function() {
-    stream.appendChild(items[0]);
-    items = document.querySelectorAll('.gallery__item');
-});
+
+   this.next = function () {
+     that.stream.appendChild(that.items[0]);
+     that.items = document.querySelectorAll('.gallery__item');
+   };
+
+   this.prev = function () {
+     that.stream.insertBefore(that.items[that.items.length - 1], that.items[0]);
+     that.items = document.querySelectorAll('.gallery__item');
+   };
+};
+
+const minhaGaleria = new galeria();
+
+minhaGaleria.btnPrev.addEventListener('click', minhaGaleria.prev);
+
+minhaGaleria.btnNext.addEventListener('click', minhaGaleria.next);
